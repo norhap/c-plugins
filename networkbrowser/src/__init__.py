db5d92c23c8626d423ad67b772236f0fd3849b27
persianpros <persianpros@gmail.com>
2021-08-29 12:57:09 +0430

    Add localeInit() for new LocaleSelection and International code

--- networkbrowser/src/__init__.py
+++ networkbrowser/src/__init__.py
@@ -30,4 +30,5 @@ def _(txt):
 		return gettext.gettext(txt)
 
 
-language.addCallback(localeInit())
+localeInit()
+language.addCallback(localeInit)
--- permanenttimeshift/src/plugin.py
+++ permanenttimeshift/src/plugin.py
@@ -66,7 +66,8 @@ def _(txt):
 		return gettext.gettext(txt)
 
 
-language.addCallback(localeInit())
+localeInit()
+language.addCallback(localeInit)
 
 ##############################
 #####  CONFIG SETTINGS   #####
